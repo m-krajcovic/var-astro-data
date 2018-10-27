a343e123d28aa02e60e8faef9660da4381482bee
Michal Krajcovic <mich.krajcovic@gmail.com>
2018-10-27 17:44:08 +0200

    updated jpa queries to work with mac mysql

--- src/main/kotlin/cz/astro/var/data/repository/StarRepository.kt
+++ src/main/kotlin/cz/astro/var/data/repository/StarRepository.kt
@@ -12,12 +12,12 @@ import org.springframework.data.repository.query.Param
 interface StarRepository : JpaRepository<Star, Int> {
     fun findByConstellation(constellation: String): List<Star>
 
-    @Query("select new cz.astro.var.data.repository.ConstellationStarSummary(s.constellation, COUNT(s)) from Star s GROUP BY s.constellation ORDER BY s.constellationId")
+    @Query("select new cz.astro.var.data.repository.ConstellationStarSummary(s.constellation, COUNT(s)) from Star s GROUP BY s.constellationId, s.constellation ORDER BY s.constellationId")
     fun findAllConstellationStarSummary(): Set<ConstellationStarSummary>
 
-    @Query("select new cz.astro.var.data.repository.StarMinimaSummary(s.id, s.constellation, s.starName, COUNT(m)) from StarMinima m JOIN Star s ON m.constellationId = s.constellationId AND m.starId = s.starId GROUP BY s.id, s.constellation, s.starName ORDER BY s.starId")
+    @Query("select new cz.astro.var.data.repository.StarMinimaSummary(s.id, s.constellation, s.starName, COUNT(m)) from StarMinima m JOIN Star s ON m.constellationId = s.constellationId AND m.starId = s.starId GROUP BY s.id, s.starId, s.constellation, s.starName ORDER BY s.starId")
     fun findAllStarMinimaSummary(): List<StarMinimaSummary>
 
-    @Query("select new cz.astro.var.data.repository.StarMinimaSummary(s.id, s.constellation, s.starName, COUNT(m)) from StarMinima m JOIN Star s ON m.constellationId = s.constellationId AND m.starId = s.starId WHERE s.constellation = :constellation GROUP BY s.id, s.constellation, s.starName ORDER BY s.starId")
+    @Query("select new cz.astro.var.data.repository.StarMinimaSummary(s.id, s.constellation, s.starName, COUNT(m)) from StarMinima m JOIN Star s ON m.constellationId = s.constellationId AND m.starId = s.starId WHERE s.constellation = :constellation GROUP BY s.id, s.starId, s.constellation, s.starName ORDER BY s.starId")
     fun findStarMinimaSummaryByConstellation(@Param("constellation") constellation: String): List<StarMinimaSummary>
 }
