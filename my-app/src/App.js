f68f6ff1617c454796b4c2b4e3fb8352090437f9
Michal Krajcovic <mich.krajcovic@gmail.com>
2018-11-17 02:25:53 +0100

    starting czev frontend; added antd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,75 @@
 import React, {Component} from 'react';
-import './App.css';
-import './components/ocgate/StarList.css';
+// import './App.css';
+// import './components/ocgate/StarList.css';
 import ConstellationList from "./components/ocgate/ConstellationList";
 import {BASE_URL} from "./api-endpoint";
 import StarList from "./components/ocgate/StarList";
 import StarDetail from "./components/ocgate/StarDetail";
-import { BrowserRouter as Router, Route, Redirect, NavLink } from "react-router-dom";
+import {BrowserRouter as Router, Route, Redirect, NavLink, Switch, withRouter} from "react-router-dom";
 import Czev from "./components/czev/Czev";
+
+
+import "./App.css"
+import "antd/dist/antd.css";
+
+import {Layout, Menu, Breadcrumb} from 'antd';
+
+const {Header, Content, Footer} = Layout;
+
+const LinkMenu = withRouter(props => {
+        const {location} = props;
+        return (
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                style={{lineHeight: '64px'}}
+                selectedKeys={[location.pathname]}
+            >
+                <Menu.Item key="/oc">
+                    <NavLink to="/oc"><span className="header-link">O-C Gate</span></NavLink>
+                </Menu.Item>
+                <Menu.Item key="/czev">
+                    <NavLink to="/czev"><span className="header-link">CzeV</span></NavLink>
+                </Menu.Item>
+            </Menu>
+        );
+    }
+);
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <div className="app">
-                    <div className="app-header">
-                        <NavLink to="/oc" activeClassName="active"><span className="header-link">O-C Gate</span></NavLink>
-                        <NavLink to="/czev" activeClassName="active"><span className="header-link">CzeV</span></NavLink>
-                        <NavLink to="/brno" activeClassName="active"><span className="header-link">B.R.N.O.</span></NavLink>
-                        <NavLink to="/predictions" activeClassName="active"><span className="header-link">Predictions</span></NavLink>
-                    </div>
-                    <Route exact path="/oc" component={OcGate}/>
-                    <Route path="/czev" component={Czev}/>
-                    <Route path="/brno" component={Brno}/>
-                    <Route path="/predictions" component={Predictions}/>
-                    <Redirect from="/" to="oc" />
-                </div>
+                <Layout className="layout">
+                    <Header style={{position: 'fixed', zIndex: 1, width: '100%'}}>
+                        <LinkMenu/>
+                    </Header>
+                    <Content style={{marginTop: 64}}>
+                        <Switch>
+                            <Route path="/oc" component={OcGate}/>
+                            <Route path="/czev" component={Czev}/>
+                            <Route path="/brno" component={Brno}/>
+                            <Route path="/predictions" component={Predictions}/>
+                            <Redirect to="/oc"/>
+                        </Switch>
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}>
+                        Michal Krajčovič ©2018
+                    </Footer>
+                </Layout>
+                {/*<div className="app">*/}
+                {/*<div className="app-header">*/}
+                {/*<NavLink to="/oc" activeClassName="active"><span className="header-link">O-C Gate</span></NavLink>*/}
+                {/*<NavLink to="/czev" activeClassName="active"><span className="header-link">CzeV</span></NavLink>*/}
+                {/*<NavLink to="/brno" activeClassName="active"><span className="header-link">B.R.N.O.</span></NavLink>*/}
+                {/*<NavLink to="/predictions" activeClassName="active"><span className="header-link">Predictions</span></NavLink>*/}
+                {/*</div>*/}
+                {/*<Route exact path="/oc" component={OcGate}/>*/}
+                {/*<Route path="/czev" component={Czev}/>*/}
+                {/*<Route path="/brno" component={Brno}/>*/}
+                {/*<Route path="/predictions" component={Predictions}/>*/}
+                {/*<Redirect from="/" to="oc" />*/}
+                {/*</div>*/}
             </Router>
         );
     }
@@ -39,7 +84,12 @@ class OcGate extends Component {
     componentDidMount() {
         this.setState({...this.state, constellationsLoading: true});
         fetch(BASE_URL + "/oc/constellations").then(response => response.json())
-            .then(value => this.setState({constellations: value, stars: [], selectedStar: null, constellationsLoading: false}));
+            .then(value => this.setState({
+                constellations: value,
+                stars: [],
+                selectedStar: null,
+                constellationsLoading: false
+            }));
     }
 
     onConstellationSelected(constellation) {
@@ -60,14 +110,18 @@ class OcGate extends Component {
             <div className="oc-gate-app">
                 <div className="sidebar-wrapper">
                     <div className="sidebar">
-                        <ConstellationList constellations={this.state.constellations} onSelected={constellation => this.onConstellationSelected(constellation)} loading={this.state.constellationsLoading}/>
+                        <ConstellationList constellations={this.state.constellations}
+                                           onSelected={constellation => this.onConstellationSelected(constellation)}
+                                           loading={this.state.constellationsLoading}/>
                     </div>
                     <div className="sidebar">
-                        <StarList stars={this.state.stars} onSelected={star => this.onStarSelected(star)} loading={this.state.starsLoading}/>
+                        <StarList stars={this.state.stars} onSelected={star => this.onStarSelected(star)}
+                                  loading={this.state.starsLoading}/>
                     </div>
                 </div>
                 <div className="stars-detail-wrapper">
-                    <StarDetail selectedElement={this.state.selectedElement} star={this.state.selectedStar} loading={this.state.starLoading} onElementChange={(element) => {
+                    <StarDetail selectedElement={this.state.selectedElement} star={this.state.selectedStar}
+                                loading={this.state.starLoading} onElementChange={(element) => {
                         this.setState({...this.state, selectedElement: element})
                     }}/>
                 </div>
@@ -75,7 +129,6 @@ class OcGate extends Component {
         );
     }
 }
-
 
 
 class Brno extends Component {
