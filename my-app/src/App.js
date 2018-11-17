c3abb6ea1206d2e1eb0640e6c002ebaeb784b7a1
Michal Krajcovic <mich.krajcovic@gmail.com>
2018-11-17 20:54:56 +0100

    started czev app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,9 +12,9 @@ import Czev from "./components/czev/Czev";
 import "./App.css"
 import "antd/dist/antd.css";
 
-import {Layout, Menu, Breadcrumb} from 'antd';
+import {Layout, Menu } from 'antd';
 
-const {Header, Content, Footer} = Layout;
+const {Header, Content, Footer, Sider} = Layout;
 
 const LinkMenu = withRouter(props => {
         const {location} = props;
@@ -40,11 +40,10 @@ class App extends Component {
     render() {
         return (
             <Router>
-                <Layout className="layout">
-                    <Header style={{position: 'fixed', zIndex: 1, width: '100%'}}>
+                <Layout className="layout" style={{minHeight: "100vh"}}>
+                    <Header style={{width: "100%"}}>
                         <LinkMenu/>
                     </Header>
-                    <Content style={{marginTop: 64}}>
                         <Switch>
                             <Route path="/oc" component={OcGate}/>
                             <Route path="/czev" component={Czev}/>
@@ -52,24 +51,10 @@ class App extends Component {
                             <Route path="/predictions" component={Predictions}/>
                             <Redirect to="/oc"/>
                         </Switch>
-                    </Content>
                     <Footer style={{textAlign: 'center'}}>
                         Michal Krajčovič ©2018
                     </Footer>
                 </Layout>
-                {/*<div className="app">*/}
-                {/*<div className="app-header">*/}
-                {/*<NavLink to="/oc" activeClassName="active"><span className="header-link">O-C Gate</span></NavLink>*/}
-                {/*<NavLink to="/czev" activeClassName="active"><span className="header-link">CzeV</span></NavLink>*/}
-                {/*<NavLink to="/brno" activeClassName="active"><span className="header-link">B.R.N.O.</span></NavLink>*/}
-                {/*<NavLink to="/predictions" activeClassName="active"><span className="header-link">Predictions</span></NavLink>*/}
-                {/*</div>*/}
-                {/*<Route exact path="/oc" component={OcGate}/>*/}
-                {/*<Route path="/czev" component={Czev}/>*/}
-                {/*<Route path="/brno" component={Brno}/>*/}
-                {/*<Route path="/predictions" component={Predictions}/>*/}
-                {/*<Redirect from="/" to="oc" />*/}
-                {/*</div>*/}
             </Router>
         );
     }
@@ -107,26 +92,24 @@ class OcGate extends Component {
 
     render() {
         return (
-            <div className="oc-gate-app">
-                <div className="sidebar-wrapper">
-                    <div className="sidebar">
-                        <ConstellationList constellations={this.state.constellations}
-                                           onSelected={constellation => this.onConstellationSelected(constellation)}
-                                           loading={this.state.constellationsLoading}/>
-                    </div>
-                    <div className="sidebar">
-                        <StarList stars={this.state.stars} onSelected={star => this.onStarSelected(star)}
-                                  loading={this.state.starsLoading}/>
-                    </div>
-                </div>
-                <div className="stars-detail-wrapper">
+            <Content>
+                <Sider theme="light" style={{overflow: 'auto', height: 'calc(100vh - 64px)', position: 'fixed',top:64, left: 0}}>
+                    <ConstellationList constellations={this.state.constellations}
+                                       onSelected={constellation => this.onConstellationSelected(constellation)}
+                                       loading={this.state.constellationsLoading}/>
+                </Sider>
+                <Sider theme="light" style={{overflow: 'auto', height: 'calc(100vh - 64px)', position: 'fixed',top:64, left: 200}}>
+                    <StarList stars={this.state.stars} onSelected={star => this.onStarSelected(star)}
+                              loading={this.state.starsLoading}/>
+                </Sider>
+                <div className="stars-detail-wrapper" style={{overflow: 'auto', height: 'calc(100vh - 64px)', position: 'fixed', top:64, left: 400, right: 0}}>
                     <StarDetail selectedElement={this.state.selectedElement} star={this.state.selectedStar}
                                 loading={this.state.starLoading} onElementChange={(element) => {
                         this.setState({...this.state, selectedElement: element})
                     }}/>
                 </div>
-            </div>
-        );
+            </Content>
+        )
     }
 }
 
--- my-app/src/components/czev/Czev.js
+++ my-app/src/components/czev/Czev.js
@@ -1,11 +1,113 @@
 import {Component} from "react";
 import React from "react";
 
+import {Card, Layout, Menu, Table} from 'antd';
+import {BASE_URL} from "../../api-endpoint";
+import axios from "axios";
+
+const {Header, Content, Footer, Sider} = Layout;
+
 export default class Czev extends Component {
     render() {
         return (
-            <div>This will be new czev catalog!</div>
+            <Content style={{margin: "24px 24px 0"}}>
+                <Card style={{width: "100%"}}>
+                    <CzevCatalogue></CzevCatalogue>
+                </Card>
+            </Content>
         )
     }
 }
 
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'czevId',
+    },
+    {
+        title: 'Constellation',
+        dataIndex: 'constellation.name',
+    },
+    {
+        title: 'Type',
+        dataIndex: 'type',
+    },
+    {
+        title: 'RA (J2000)',
+        dataIndex: 'coordinates.ra',
+    },
+    {
+        title: 'DEC (J2000)',
+        dataIndex: 'coordinates.dec',
+    },
+    {
+        title: 'Epoch',
+        dataIndex: 'm0',
+    },
+    {
+        title: 'Period',
+        dataIndex: 'period',
+    },
+    {
+        title: 'Discoverer',
+        dataIndex: 'discoverers',
+        render: discoverers => (
+            discoverers.map(d => d.abbreviation).join(', ')
+        )
+    }
+];
+
+export class CzevCatalogue extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: [], loading: true, pagination: {}}
+    }
+
+    loadPage(page) {
+        this.setState({...this.state, loading: true});
+        axios.get(BASE_URL + "/czev/stars", {
+            params: {
+                page: page,
+                size: 10
+            }
+        }).then(value => this.setState({
+            ...this.state, data: value.data.content, loading: false,
+            pagination: {
+                total: value.data.totalElements,
+                current: value.data.number + 1,
+                size: 'small',
+                pageSize: value.data.size,
+            }
+        }));
+    }
+
+    handleTableChange = (pagination, filters, sorter) => {
+        console.log(pagination);
+        this.loadPage(pagination.current - 1)
+    };
+
+    componentDidMount() {
+        this.loadPage(0)
+    }
+
+    render() {
+        return (
+            <Table size="small" rowKey="czevId" columns={columns} dataSource={this.state.data}
+                   loading={this.state.loading} pagination={this.state.pagination} onChange={this.handleTableChange}/>
+            // <CzevCatalogueTable loading={this.state.loading} data={this.state.data} pagination={this.state.pagination}/>
+        )
+    }
+}
+
+export class CzevCatalogueTable extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <Table size="small" rowKey="czevId" columns={columns} dataSource={this.props.data}
+                   loading={this.props.loading} pagination={this.props.pagination}/>
+        )
+    }
+}
