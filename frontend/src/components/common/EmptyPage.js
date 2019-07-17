01cdb26c9591e1a8a97d235642ab4885deba4f7c
Michal Krajcovic <mich.krajcovic@gmail.com>
2019-07-17 09:17:54 +0200

    added nav menus for admin pages

---
+++ frontend/src/components/common/EmptyPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function EmptyPage() {
+    return (
+        <div style={{position: "absolute", top: "50%", textAlign: "center", width: "100%"}}>This is not the page you are looking for.</div>
+    )
+}
